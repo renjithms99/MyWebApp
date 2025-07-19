c3f4dcca2dc247f4301a6323d1fc97cf473efa99
Renjith ms <renjithms99@gmail.com>
2025-07-19 17:06:33 +0530

    removed the open ai references

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -6,20 +6,12 @@ namespace MyWebApp.Pages;
 public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
-    private readonly OpenAIChatService _chatService;
-    public string Response { get; set; }
 
     public IndexModel(ILogger<IndexModel> logger)
     {
         _logger = logger;
-        _chatService = new OpenAIChatService();
     }
 
-    public async Task<IActionResult> OnPostAsync(string userMessage)
-    {
-        Response = await _chatService.GetChatResponse(userMessage);
-        return Page();
-    }
 
     public void OnGet()
     {
